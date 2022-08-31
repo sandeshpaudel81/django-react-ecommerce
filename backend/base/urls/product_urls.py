13a6ddf262e10f461219775df13d436a8d85555d
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-08-31 23:48:54 +0545

    add and update product

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,8 +4,9 @@ from base.views import product_views as views
 urlpatterns = [
 
     path('', views.getProducts, name="products"),
+    path('create/', views.createProduct, name='product-create'),
     path('<str:pk>/', views.getProduct, name="product"),
-
-    path('delete/<str:pk>/', views.deleteProduct, name='product-delete')
+    path('delete/<str:pk>/', views.deleteProduct, name='product-delete'),
+    path('update/<str:pk>/', views.updateProduct, name='product-update'),
 
 ]
