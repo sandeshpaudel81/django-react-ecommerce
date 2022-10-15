8f1dbdf7d8c6671ceed623c9cc5fcceb1b4c000d
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-15 22:07:27 +0545

    order urls

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from base.views import order_views as views
 
+# url routes related to order
 
 urlpatterns = [
 
-    path('add/', views.addOrderItems, name="orders-add"),
-    path('myorders/', views.getMyOrders, name='myorders'),
+    path('add/', views.addOrderItems, name="orders-add"), # to add order items
+    path('myorders/', views.getMyOrders, name='myorders'), # to view my order
 
-    path('<str:pk>/', views.getOrderById, name="user-order"),
-    path('<str:pk>/pay/', views.updateOrderToPaid, name="pay"),
+    path('<str:pk>/', views.getOrderById, name="user-order"), # to get order by id for admin
+    path('<str:pk>/pay/', views.updateOrderToPaid, name="pay"), # add payment to the order
 ]
