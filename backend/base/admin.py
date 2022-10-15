fd69f410bb79ba959324bdafcf7cc1cd5f1217d6
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-15 20:53:15 +0545

    comments on admin py

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Review)
-admin.site.register(ShippingAddress)
+
+admin.site.register(Product) # registering Product model in admin dashboard
+admin.site.register(Order) # registering Order model in admin dashboard
+admin.site.register(OrderItem) # registering OrderItem model in admin dashboard
+admin.site.register(Review) # registering Review model in admin dashboard
+admin.site.register(ShippingAddress) # registering ShippingAddress model in admin dashboard
