a2ab73b82c7d786f93f1f4d743021348a01f9b86
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-16 14:42:22 +0545

    wsgi file

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
-
+# This file is used to forward requests from server to the backend application
 import os
 
 from django.core.wsgi import get_wsgi_application
