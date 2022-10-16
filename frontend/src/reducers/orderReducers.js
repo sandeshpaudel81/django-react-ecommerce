4b98476b893661bb4281f3b8c40db7b63b7746fc
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-16 19:46:02 +0545

    order reducer

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,3 +1,4 @@
+// importing constants
 import { 
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
@@ -19,26 +20,34 @@ import {
     ORDER_LIST_MY_RESET,
  } from '../constants/orderConstants'
 
+// Reducers are the functions that contain logic and calculations that needed to be performed on the state
+// according to action type passed by actions functions
+
+// reducer to create new order and initializing orderCreate state as null
 export const orderCreateReducer = (state={}, action) => {
     switch(action.type){
+        // if action.type sent by action function == ORDER_CREATE_REQUEST
         case ORDER_CREATE_REQUEST:
             return {
-                 loading: true
+                 loading: true // loading before api call
             }
 
+        // when order create api request is succeeded    
         case ORDER_CREATE_SUCCESS:
             return {
                 loading: false,
                 success: true,
-                order: action.payload
+                order: action.payload // data fetched from api response
             }
 
+        // if api response sends error    
         case ORDER_CREATE_FAIL:
             return {
                 loading: false,
-                error: action.payload
+                error: action.payload // error message from api response
             }
 
+        // this is called after order is placed    
         case ORDER_CREATE_RESET:
             return {}
 
@@ -47,24 +56,28 @@ export const orderCreateReducer = (state={}, action) => {
     }
 }
 
+// reducer to fetch order details and initializing loading as true, order-items and shipping address as null
 export const orderDetailsReducer = (state={loading: true, orderItems:[], shippingAddress:{}}, action) => {
     switch(action.type){
+        // before api is called to fetch data
         case ORDER_DETAILS_REQUEST:
             return {
-                ...state,
+                ...state, // copy of the previous state
                 loading: true
             }
 
+        // after api fetch is succeeded    
         case ORDER_DETAILS_SUCCESS:
             return {
-                loading: false,
-                order: action.payload
+                loading: false, 
+                order: action.payload // order details fetched from api response
             }
 
+        // if api response sends error    
         case ORDER_DETAILS_FAIL:
             return {
                 loading: false,
-                error: action.payload
+                error: action.payload // error message from api response
             }
 
         default:
@@ -73,13 +86,16 @@ export const orderDetailsReducer = (state={loading: true, orderItems:[], shippin
 }
 
 
+// reducer to pay the order
 export const orderPayReducer = (state={}, action) => {
     switch(action.type){
+        // before api is called to fetch data
         case ORDER_PAY_REQUEST:
             return {
                 loading: true
             }
 
+        // after api fetch is succeeded
         case ORDER_PAY_SUCCESS:
             return {
                 loading: false,
@@ -101,6 +117,7 @@ export const orderPayReducer = (state={}, action) => {
 }
 
 
+// reducer to list my orders with initialization of orders as null
 export const orderListMyReducer = (state={orders:[]}, action) => {
     switch(action.type){
         case ORDER_LIST_MY_REQUEST:
@@ -108,16 +125,17 @@ export const orderListMyReducer = (state={orders:[]}, action) => {
                 loading: true
             }
 
+        // after api fetch is succeeded
         case ORDER_LIST_MY_SUCCESS:
             return {
                 loading: false,
-                orders: action.payload
+                orders: action.payload // list of orders fetched from api response
             }
 
         case ORDER_LIST_MY_FAIL:
             return {
                 loading: false,
-                error: action.payload
+                error: action.payload // error message from api response
             }
 
         case ORDER_LIST_MY_RESET:
